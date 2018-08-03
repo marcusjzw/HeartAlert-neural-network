96f72823392b69300cc7280497d596e020ac231f
Marcus Wong <marcuswong@hotmail.com>
2018-08-03 14:51:38 +1200

    app now shows HR, successful connection with belt

---
+++ PolarHeartRateApplication/app/src/main/java/com/mwon724/heartalert/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.mwon724.heartalert;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mwon724.heartalert.R;
+
+public class AboutActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+}
