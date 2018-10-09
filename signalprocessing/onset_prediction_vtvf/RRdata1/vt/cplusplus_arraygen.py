443ba5d4f741cc300edb1e019bf0f56d6eaecb9d
Marcus Wong <marcuswong@hotmail.com>
2018-10-10 11:43:54 +1300

    finished script to produce values

--- signalprocessing/onset_prediction_vtvf/RRdata1/vt/cplusplus_arraygen.py
+++ signalprocessing/onset_prediction_vtvf/RRdata1/vt/cplusplus_arraygen.py
@@ -1,2 +1,9 @@
-for line in file('/Users/Marcus/git/compsys700-new/signalprocessing/onset_prediction_vtvf/RRdata1/vt'): # CHANGE THIS FILE IF WANT TO RUN
-    print line.replace('\n', ',')
+filename = '77_.txt'
+codesnippet = 'static uint8_t rriValues = {'
+for line in file('/Users/Marcus/git/compsys700-new/signalprocessing/onset_prediction_vtvf/RRdata1/vt/'+filename): # CHANGE THIS FILE IF WANT TO RUN
+    codesnippet += line.rstrip() + ','
+
+codesnippet = codesnippet[:-1] # remove ending comma
+codesnippet += '}'
+print(codesnippet)
+
